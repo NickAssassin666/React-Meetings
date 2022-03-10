960616595069044192dbaccd0a95382f407918b2
NickAssassin666 <84249423+NickAssassin666@users.noreply.github.com>
2022-03-10 20:16:48 +0530

    redirect React-Meetings to /

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import AddMeetings from "./pages/AddMeetings";
 import AllMeetings from "./pages/AllMeetings";
 import Favorites from "./pages/Favorites";
-import Layout from "./components/layout/Layout"
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <Layout>
       <Switch>
-        <Route path="/" exact>
-          <AllMeetings/>
+        <Route path="/React-Meetings">
+          <Redirect to="/" />
         </Route>
-        <Route path="/new-meetings"><AddMeetings /></Route>
-        <Route path="/favorites"><Favorites /></Route>
+        <Route path="/" exact>
+          <AllMeetings />
+        </Route>
+        <Route path="/new-meetings">
+          <AddMeetings />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
       </Switch>
     </Layout>
   );
